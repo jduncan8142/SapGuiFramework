085f1b8f4f6ed3c2fb5e0c1ca5fbe49a46f32f98
Jason <jason.matthew.duncan@gmail.com>
2021-11-03 12:31:03 -0500

    bug fix

--- SapGui/SapGui.py
+++ SapGui/SapGui.py
@@ -18,7 +18,7 @@ class SapLogger:
         self.enabled: bool = conf.enable
         self.log_name: str = log_name if log_name is not None else conf.name
         self.log_path: str = log_path if log_path is not None else conf.path
-        self.log_file: str = os.path.join([self.log_path, f"{self.log_name}.log"])
+        self.log_file: str = os.path.join(self.log_path, f"{self.log_name}.log")
         self.log: logging.Logger = logging.getLogger(self.log_file)
         self.formatter: logging.Formatter = logging.Formatter(conf.format)
         self.file_handler: logging.FileHandler = logging.FileHandler(self.log_file, mode=conf.file_mode)
